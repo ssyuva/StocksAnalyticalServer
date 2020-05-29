ca6b1cabfc2141ffa97970e4e30a7c65913eb046
Yuvaraja Subramaniam <subramaniam.yuvaraja@gmail.com>
2020-05-29 12:10:24 +0530

    Fixed the issues with processing trade_packet_arrival event

--- AnalyticalServer.cpp
+++ AnalyticalServer.cpp
@@ -29,6 +29,24 @@ struct tradepacket {
 	uint64_t ts2;
 };
 
+
+//15 mins Bar Context
+struct BarCntxt {
+	char         sym[15];
+	unsigned int bar_num;
+	uint64_t     bar_start_time;
+	uint64_t     bar_close_time;
+	double		 bar_open;
+	double       bar_high;
+	double       bar_low;
+	double       bar_close;
+	double       bar_volume;
+};
+
+//Bar Types
+enum Bar_Type {CLOSING_BAR = 0, TRADE_BAR = 1, BAR_TYPE_COUNT};
+vector<string> Bar_Type_Name = { "CLOSING_BAR", "TRADE_BAR", "BAR_TYPE_INVALID" };
+
 //FSM States
 enum FSM_States {FSM_STARTING = 0, FSM_READY = 1, FSM_DOWN = 2, FSM_STATE_COUNT};
 vector<string> FSM_State_Name = { "FSM_STARTING", "FSM_READY", "FSM_DOWN", "FSM_STATE_INVALID" };
@@ -76,6 +94,7 @@ bool process_fsm_down(FSM_EVENT & fsm_ev);
 bool process_fsm_ready_ev_trd_pkt_arrival(FSM_EVENT & fsm_ev);
 bool process_fsm_ready_ev_tmr_expiry(FSM_EVENT & fsm_ev);
 bool process_fsm_event(FSM_EVENT & fsm_ev);
+bool fsm_emit_bar(BarCntxt barcntxt, Bar_Type bt);
 
 vector<string> tokenize(const char *str, char c);
 bool parse_trade(string line, map<string, string> & trdmap);
@@ -88,7 +107,11 @@ FSM_EVENT_HANDLER FSM_Ev_Handler_Table[FSM_STATE_COUNT][EVENT_TYPE_COUNT] = {
 
 
 int pfd[2];
+
 FSM_States fsm_curr_state = FSM_STARTING;
+map<string, BarCntxt> bar_cntxt_cache;
+
+const uint64_t fifteen_min_millisecs = 15 * 60 * 1000 * 10 * 100 ;
 
 int main()
 {
@@ -222,21 +245,6 @@ vector<string> tokenize(const char *str, char c)
 
 
 
-//Ticker Context
-struct TickerCtxt {
-	unsigned int bar_num;
-	uint64_t     start_time;
-	uint64_t     close_time;
-	double		 bar_open;
-	double       bar_high;
-	double       bar_low;
-	double       bar_close;
-	double       bar_volume;
-};
-
-
-
-
 //Thread 2: FSM thread. Reads trade packets from Worker 1 and calculates bar OHLC values
 void *fsm_thread_bar_calc(void *msg)
 {
@@ -308,6 +316,7 @@ bool process_fsm_down(FSM_EVENT & fsm_ev) {
 return true;
 }
 
+
 //Process events TRADE_PKT_ARRIVAL while FSM_State == FSM_READY
 bool process_fsm_ready_ev_trd_pkt_arrival(FSM_EVENT & fsm_ev) {
 	
@@ -319,8 +328,131 @@ bool process_fsm_ready_ev_trd_pkt_arrival(FSM_EVENT & fsm_ev) {
 
 	cout << "FSM Thread => event arrived = trd_pkt_arrival: sym = " << symbol << ", P = " << price << ", Q = " << qty << ", TS2 = " << ts2 << endl;
 
+	//check if symbol exists in Bar contexts cache
+	string sym(symbol);
+
+	cout << "FSM Thread => Searching bar cache for symbol " << symbol << endl;
+
+	auto it = bar_cntxt_cache.find(sym);
+
+	bool cntxt_exists = (it != bar_cntxt_cache.end());
+
+	cout << "FSM Thread => cntxt_exists = " << cntxt_exists << endl;
+
+	if (!cntxt_exists) {
+		//bars context does not exist. create it
+	    cout << "FSM Thread => Bar context does not exist. Creating it : sym = " << symbol << endl;
+		BarCntxt newcntxt;
+		strcpy(newcntxt.sym, symbol);
+		newcntxt.bar_num        = 1;
+		newcntxt.bar_start_time = ts2;
+		newcntxt.bar_close_time = ts2 + fifteen_min_millisecs;
+		newcntxt.bar_open       = price;
+		newcntxt.bar_high       = price;
+		newcntxt.bar_low        = price;
+		newcntxt.bar_close      = price;
+		newcntxt.bar_volume     = qty;
+	
+		//store the new context in cache
+		bar_cntxt_cache.insert( pair<string, BarCntxt>(sym, newcntxt) );
+		//TODO - update subscribers on bar open
+	}
+	else {
+		//bars context exist, update it
+	    cout << "FSM Thread => Bar context exists. Update it : sym = " << symbol << endl;
+		BarCntxt oldcntxt = it->second;
+	    cout << "FSM Thread => sym = " << symbol << ", Current bar close time = " << oldcntxt.bar_close_time << ", Current TS2 : " << ts2 << endl;
+		if ( ts2 <= oldcntxt.bar_close_time) {
+	    cout << "FSM Thread => sym = " << symbol << ". Trade goes into exising bar" << endl;
+		//TODO - remove debug
+		fsm_emit_bar(oldcntxt, TRADE_BAR);
+
+			//trade goes into existing bar
+			if (price > oldcntxt.bar_high) {
+				oldcntxt.bar_high = price;
+			}
+
+			if (price < oldcntxt.bar_low ) {
+				oldcntxt.bar_low  = price;
+			}
+
+			oldcntxt.bar_close    = price;
+			oldcntxt.bar_volume  += qty;
+			it->second = oldcntxt;
+		
+		//TODO - remove debug
+		fsm_emit_bar(oldcntxt, TRADE_BAR);
+
+			//TODO - update subscribers on trade update
+		}
+		else {
+			    //trade goes into next bar or someother future bar
+	    		cout << "FSM Thread => sym = " << symbol << ". Trade goes into next bar or future bar" << endl;
+				uint64_t curr_bar_close_time = oldcntxt.bar_close_time;
+				do {
+					// keep closing the current bar until the bar that accomodates the current trade opens up
+					BarCntxt newcntxt;
+					strcpy(newcntxt.sym, symbol);
+					newcntxt.bar_num        = oldcntxt.bar_num + 1;
+					newcntxt.bar_start_time = oldcntxt.bar_close_time + 1;
+					newcntxt.bar_close_time = newcntxt.bar_start_time + fifteen_min_millisecs;
+					newcntxt.bar_open       = oldcntxt.bar_close;
+					newcntxt.bar_high       = oldcntxt.bar_close;
+					newcntxt.bar_low        = oldcntxt.bar_close;
+					newcntxt.bar_close      = oldcntxt.bar_close;
+					newcntxt.bar_volume     = 0;
+
+					//emit closing bar info to worker 3 
+					fsm_emit_bar(oldcntxt, CLOSING_BAR);
+
+					//update the bar cache with current bar info
+					it->second = newcntxt;
+					curr_bar_close_time = newcntxt.bar_close_time;
+					oldcntxt = newcntxt;
+
+				} while (ts2 > curr_bar_close_time ); 
+				
+				//the intermediate bars have been closed. update the current bar with current trade info
+				oldcntxt.bar_open     = price;
+				oldcntxt.bar_high     = price;
+				oldcntxt.bar_low      = price;
+				oldcntxt.bar_close    = price;
+				oldcntxt.bar_volume  += qty;
+				it->second = oldcntxt;
+				fsm_emit_bar(oldcntxt, TRADE_BAR);
+				//TODO - update subscribers on trade update
+		}
+	}
 return true;
 }
+
+
+//Emit bar into to worker 3
+bool fsm_emit_bar(BarCntxt barcntxt, Bar_Type bt){
+	char symbol[15];
+	unsigned int bar_num   = barcntxt.bar_num;
+	double bar_open        = barcntxt.bar_open;
+	double bar_high        = barcntxt.bar_high;
+	double bar_low         = barcntxt.bar_low;
+	double bar_close       = barcntxt.bar_close;
+	double bar_volume      = barcntxt.bar_volume;
+	uint64_t bar_start_time  = barcntxt.bar_start_time;
+	uint64_t bar_close_time  = barcntxt.bar_close_time;
+	strcpy(symbol, barcntxt.sym);
+	cout << "FSM Thread => Emiting Bar : " 
+	             << "bartype = "          << Bar_Type_Name[bt] 
+	             << ", symbol = "         << symbol 
+	             << ", bar_num = "        << bar_num
+	             << ", O = "              << bar_open
+	             << ", H = "              << bar_high
+	             << ", L = "              << bar_low
+	             << ", C = "              << bar_close
+	             << ", volume = "         << bar_volume
+	             << ", bar_start_time = " << bar_start_time
+	             << ", bar_close_time = " << bar_close_time
+	             << endl;
+}
+
 
 //Process events TIMER_EXPIRY while FSM_State == FSM_READY
 bool process_fsm_ready_ev_tmr_expiry(FSM_EVENT & fsm_ev) {
